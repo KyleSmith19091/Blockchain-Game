f4a0332c90832856fd8a999c50ce4c21c15ff222
Kyle Smith <sKorpion19091>
2020-06-21 16:29:13 +0200

    Cleaned up main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,44 +9,89 @@
 
 using namespace std;
 
-int main(){
+bool findWinner(Block hacker, Block cyber, int diff){
+    bool result = true;
+    int nonceHacker = hacker.MineBlock(diff);
+    int nonceCyber = hacker.MineBlock(diff);
+    if(nonceHacker < nonceCyber){
+        cout << "System Hacked" << endl;
+        cout << "Transaction Comprimised!" << endl;
+        result = false;
+    }else{
+        cout << "System hack stopped!" << endl;
+        cout << "Cyber force defence successful!" << endl;
+    }
+    return result;
+}
 
-    Blockchain bc = Blockchain();
-    User u = startup::DisplayWelcome();
-    cout << u.getIsHacker() << endl;
-    int chainSize = rand() % 20 + 10;
-    cout << chainSize << endl;
-    system("clear");
-    cout << "INCOMING TRANSATION!" << endl;
-    cout << "Will you initiate the hack?" << endl;
-    cout << "Waiting for User Confirmation!" << endl;
+void showHackerScreen(){
+    cout << "> Will you initiate the hack?" << endl;
+    cout << "> Waiting for User Confirmation!";
     cin.get();
-    cout << "INITIATING HACK!" << endl;
+    cout << "\n";
+    cout << "~ INITIATING HACK!" << endl;
     this_thread::sleep_for(chrono::seconds(1));
-    cout << "Cracking Encryption!" << endl;  
+    cout << "~ Cracking Encryption!" << endl;  
     this_thread::sleep_for(chrono::seconds(1));
-    cout << "Done!" << endl;
+    cout << "~ Done!" << endl;
     this_thread::sleep_for(chrono::seconds(1));
-    cout << "Bypassing Spiders" << endl;
+    cout << "~ Bypassing Spiders" << endl;
     this_thread::sleep_for(chrono::seconds(1));
-    cout << "DONE!" << endl;
+    cout << "~ DONE!" << endl;
+    this_thread::sleep_for(chrono::seconds(1));
+    cout << "~ Possible Transaction Found!" << endl;
+    this_thread::sleep_for(chrono::seconds(1));
+    cout << "~ Polycode algorithm initiated!" << endl;
+    this_thread::sleep_for(chrono::seconds(1));
+    cout << "~ Mining Transaction!" << endl;
+    this_thread::sleep_for(chrono::seconds(1));
     int money = rand() % 10000 + 100;
     int diff = rand() % 2 + 1; // Max difficulty of 10
     Block hacker(1, to_string(money));
-    Block cyber(2,to_string(money));
-    cout << "Block Difficulty: " << diff << endl;
+    Block cyber(2, to_string(money));
+    bool winner = findWinner(hacker, cyber, diff);
+    if(!winner) { cout << "VarCoin Transaction intercepted\n" << "§ " << money << " taken!" << endl;}
+}
+
+void showPlayerScreen(){
+    cout << "! INTRUSION DETECTED !" << endl;
+    cout << "> Will You Intercept?" << endl;
+    cout << "> Waiting for User Confirmation!";
+    cin.get();
+    cout << "\n";
+    cout << "~ STARTING COUNTER-MEASURES!" << endl;
     this_thread::sleep_for(chrono::seconds(1));
-    int attempts = hacker.MineBlock(diff);
-    int cyberAttempt = cyber.MineBlock(diff); 
+    cout << "~ Searching for Open Port" << endl;
     this_thread::sleep_for(chrono::seconds(1));
-    cout << "Hacker: " << attempts << endl;
-    cout << "Cyber: " << cyberAttempt << endl;
-    if(attempts < cyberAttempt){
-        cout << "Kaching! \n" << "VarCoins Gained: " << money << endl;
+    cout << "~ Found port" << endl;
+    this_thread::sleep_for(chrono::seconds(1));
+    cout << "~ Burning Firewall!" << endl;
+    this_thread::sleep_for(chrono::seconds(1));
+    cout << "~ Firewall Down!" << endl;
+    this_thread::sleep_for(chrono::seconds(1));
+    cout << "~ Found intercepted transaction!" << endl;
+    this_thread::sleep_for(chrono::seconds(1));
+    cout << "~ Countering Polycode Algorithm!" << endl;
+    int money = rand() % 10000 + 100;
+    int diff = rand() % 2 + 1;
+    Block cyber(1, to_string(money));
+    Block hacker(2, to_string(money));
+    bool winner = findWinner(hacker, cyber, diff);
+    if(winner) { cout << "Transaction of §" << money << " went through\nWell done cyber force 1" << endl;}
+
+}
+
+int main(){
+
+    Blockchain bc = Blockchain();
+    system("clear");
+    User u = startup::DisplayWelcome();
+    system("clear");
+    if(u.getIsHacker()){
+        showHackerScreen();        
     }else{
-        cout << "SYSTEM ERROR, Cyber Force have Breached Firewall!\nShutting Down!" << endl;
+        showPlayerScreen();        
     }
-    
 
     return 0;
 }
